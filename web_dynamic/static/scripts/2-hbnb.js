4d1455ec48f2219a001aa924f6d58aefcfed4863
cbmarie255 <court.b1993@gmail.com>
2022-12-08 14:03:38 -0600

    changing routes

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,15 @@
+$(document).ready( () => {
+    const amenities = [];
+    $('input[type="checkbox"]').change( () => {
+      if (this.checked) {
+        amenities.push(this.id);
+      } else {
+        const idx = amenities.indexOf(this.id);
+        if (idx >= 0) {
+          amenities.splice(idx, 1);
+        };
+      };
+      $('div.Amenities h4').text('Amenities: ' + amenities.join(', '));
+    });
+  });
+  
