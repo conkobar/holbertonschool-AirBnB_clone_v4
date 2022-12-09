9c62b8ead5859dcd5ed78dffb2df5d5daeb825a6
conkobar <connorkleine@gmail.com>
2022-12-09 14:35:08 -0600

    task 3!

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
     if (this.checked) {
       amenities.push($(this).attr('data-name'));
     } else {
-      amenities.pop();
+      const idx = amenities.indexOf($(this).attr('data-name'));
+      amenities.splice(idx, 1);
     }
     $('.amenities h4').html(amenities.join(', '));
   });
 
-  const url = 'http://127.0.0.1:5001/api/v1/status/'
+  const url = 'http://0.0.0.0:5001/api/v1/status/'
   $.getJSON(url, (data) => {
     const element = document.getElementById('api_status');
     element.classToggle('available');
